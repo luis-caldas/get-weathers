824bf6caf23e310e6513eaf8150b6bf881667e21
Luis <admin@luiscaldas.com>
2022-06-24 01:23:55 +0100

    compilation on windows now working

--- compile.bat
+++ compile.bat
@@ -1,1 +1,6 @@
-pyinstaller --clean --onefile --name "Get Weathers" --add-data "fonts" --icon "weather.ico"
+@echo off
+
+:: Get the current directory
+set batdir=%~dp0
+
+pyinstaller --clean --onefile --name "Get Weathers" --add-data "%batdir%fonts\*ttf;fonts\." --icon "%batdir%weather.ico" "%batdir%main.py"
